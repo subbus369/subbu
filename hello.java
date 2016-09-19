47f8334a04a6f91ace2a675c2c7c7e777868239e
subbus369 <anji.subramanyam@gmail.com>
2016-09-19 16:44:18 +0530

    Add files via upload
    ok

---
+++ hello.java
@@ -0,0 +1,18 @@
+public class Solution2 {
+   public String backAround(String s) {
+       /*
+        * Extract the last character of the string
+          i) calculate the length of the string.
+          ii) the last index of the string can be calculated by length-1
+          iii) Use the charAt method to extract of the string
+          
+         * Append it to the beginning as well as the end of the string
+           i) Use methods in the string class to accomplish this.
+           
+         * return the new string as output
+         *
+         */
+      int len;
+      
+      len = 
+  }       
